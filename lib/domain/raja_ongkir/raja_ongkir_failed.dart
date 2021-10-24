223727f35a67b8ce9290c80e4fb9bd04f759be18
BryanJo28 <bryantjipto2324@yahoo.com>
2021-10-24 22:26:57 +0700

    push new

---
+++ lib/domain/raja_ongkir/raja_ongkir_failed.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'raja_ongkir_failed.freezed.dart';
+part 'raja_ongkir_failed.g.dart';
+
+@freezed
+class RajaOngkirFailed with _$RajaOngkirFailed {
+  factory RajaOngkirFailed({
+    int? code,
+    String? description,
+  }) = _RajaOngkirFailed;
+
+  factory RajaOngkirFailed.fromJson(Map<String, dynamic> json) =>
+      _$RajaOngkirFailedFromJson(json);
+}
---
+++ lib/domain/raja_ongkir/raja_ongkir_repository.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter2/domain/raja_ongkir/province/province_data_model.dart';
+import 'package:flutter2/domain/raja_ongkir/raja_ongkir_failed.dart';
+import 'package:flutter2/utils/constants.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class IRajaOngkir {
+  Future<Either<RajaOngkirFailed, List<ProvinceDataModel>>> getProvinceData();
+}
+
+@LazySingleton(as: IRajaOngkir)
+class RajaOngkirRepository extends IRajaOngkir {
+  Dio _dio = Dio();
+
+  @override
+  Future<Either<RajaOngkirFailed, List<ProvinceDataModel>>>
+      getProvinceData() async {
+    Response response;
+
+    _dio =
+        Dio(BaseOptions(headers: {"key": "2fdfc149871c06b7f77c6b56d4db64a2"}));
+    try {
+      response =
+          await _dio.get(Constants.rajaOngkirBaseUrl + "starter/province");
+      List<dynamic> _listData = response.data['rajaongkir']['results'];
+
+      var _listResult = _listData
+          .map((result) => ProvinceDataModel.fromJson(result))
+          .toList();
+
+      return right(_listResult);
+    } on DioError catch (err) {
+      if (err.type == DioErrorType.response) {
+        var _errorData = err.response!.data['rajaongkir']['status'];
+        var _errorModel = RajaOngkirFailed.fromJson(_errorData);
+        return left(_errorModel);
+      }
+      return left(RajaOngkirFailed());
+    }
+  }
+}
